f7e8d33169b8bcdbb9edaeea4d63836de70571b1
Debasish Sarker <debasishsarker36@gmail.com>
2021-05-18 22:40:05 +0600

    made changes to the datatypes in the models

--- src/products/models.py
+++ src/products/models.py
@@ -3,8 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-    title = models.TextField()
-    description = models.TextField()
-    price = models.TextField()
+    title = models.CharField(max_length=120) #max length required
+    description = models.TextField(blank=True, null=True)
+
+    price = models.DecimalField(decimal_places=2, max_digits=100)
     available = models.TextField()
     summary = models.TextField(default='hi there nerds')
