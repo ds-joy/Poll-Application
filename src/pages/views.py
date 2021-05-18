3ccd139a727ec86387edc31b55ca77d0ad47e853
Debasish Sarker <debasishsarker36@gmail.com>
2021-05-18 23:30:21 +0600

    created and added a about and contacts page

--- src/pages/views.py
+++ src/pages/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def home_view(response, *args, **kwargs):
     return HttpResponse("<h1>Home page<h1>")
+
+def about_view(response, *args, **kwargs):
+    return HttpResponse("<h1>About Page<h1>")
+
+def contacts_view(response, *args, **kwargs):
+    return HttpResponse("<h1>Contact Page<h1>")
